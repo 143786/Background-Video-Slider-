d774815e7468933155414b298e916ba401fb5e77
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-07-03 21:44:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Javascript of responsive navigation menu
+
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+});
