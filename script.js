c42f6dc03589532274e9c3e1c7d3224b67d0ce49
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-07-04 17:40:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navigation.classList.toggle("active");
 });
+
+// JS for video slider navigation //
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+
+var sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
